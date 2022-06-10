f302b31b50bc5459dba90b1e68525a6550dbf6d5
Calypso Leonard <calypsorleonard@gmail.com>
2022-06-10 12:48:51 -0400

    commit tree 5784a0887ccc3d73e93b2184ccad530ef31d2eb4

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -9,6 +9,36 @@ class LinkedList
   end
 
   def reverse!
-    # your code here
+    if head.nil?
+      return head
+    end   
+    
+    
+    curr = @head
+    @head = @tail
+    @tail = curr
+
+    @head = reverse_node(curr)
+    # while curr.next_node
+    #   reverse_node(curr)
+    #   curr = curr.prev_node
+    # end
+  end
+
+  def reverse_node node
+
+    if (node == nil)
+      return nil
+    end
+    temp_next = node.next_node 
+
+    node.next_node = node.prev_node
+    node.prev_node = temp_next
+
+    if (node.prev_node == nil)
+      return node
+    end
+
+    return reverse_node(node.prev_node)
   end
 end
